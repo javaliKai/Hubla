4eef62d53bdb940d27e619013598a77c5cb6578c
javaliKai <tommynicholas65@gmail.com>
2021-04-23 15:53:04 +0700

    Making landing page UI

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request, session
 
 app = Flask(__name__)
 
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
 @app.route('/')
 def home():
-    return "hello"
+    return render_template("landing.html")
+
+# I need to activate venv each time working with flask
+# to activate, venv\Scripts\activate
+# to deactivate, just type deactivate
